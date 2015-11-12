56285069a71b1ce23b24e671d5163ea60f3972a5
Hsu,HsiaoHsuan <hsuhsiaohsuan+cz80@gmail.com>
2015-11-12 19:48:08 +0800

    [2015/11/12]     1. temp.

---
+++ app/src/main/java/idv/hsu/wifiscannerlog/data/InsertDataService.java
@@ -0,0 +1,17 @@
+package idv.hsu.wifiscannerlog.data;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+/**
+ * Created by freeman on 2015/11/12.
+ */
+public class InsertDataService extends IntentService{
+    private static final String TAG = InsertDataService.class.getSimpleName();
+    private static final boolean D = true;
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+}
